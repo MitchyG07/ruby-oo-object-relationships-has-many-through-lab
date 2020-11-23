bc3866e08c051dab90efd1d17828e38b41a8e399
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-22 16:08:49 -0800

    commit tree 83dd203befbf0905b4e6e9b9004c2ca096956654

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,18 @@
+class Doctor 
+    attr_reader :name 
 
+    @@all = []
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end  
+
+    def appointments 
+        Appointment.all.select {|app| app.doctor == self}
+    end 
+
+end 
